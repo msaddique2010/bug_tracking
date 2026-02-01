fae04f38811156bf3efbf882e9d74e72e851e79d
M.Saddique <saddiquem2010@gmail.com>
2026-02-02 01:01:51 +0500

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_01_193459) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_01_195954) do
+  create_table "bugs", force: :cascade do |t|
+    t.string "bug_type"
+    t.datetime "created_at", null: false
+    t.date "deadline"
+    t.integer "project_id", null: false
+    t.string "status"
+    t.text "title"
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_bugs_on_project_id"
+  end
+
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
@@ -30,4 +41,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_01_193459) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "bugs", "projects"
 end
