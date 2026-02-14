77af430a2e974d2906de0d405f430cfeba60ce34
M.Saddique <saddiquem2010@gmail.com>
2026-02-14 19:35:34 +0500

    ....

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,5 +1,6 @@
 class Bug < ApplicationRecord
   validates :title, uniqueness: { scope: :project_id }
+  validates_presence_of :title, :status, :bug_type
   has_one_attached :image
   default_scope { order(created_at: :desc) }
   belongs_to :project
